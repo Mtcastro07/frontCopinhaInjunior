d2ce5a5a2f597f849fdc2ed99817b0e9febed940
Matheus <matheusgamer488@icloud.com>
2026-08-04 16:23:51 -0300

    feat: arquivo com componentes do projeto e header do admin implmentado

---
+++ front-copinhaIN/src/App.tsx
@@ -0,0 +1,12 @@
+import Home from "./admin/pages/Home";
+
+function App() {
+  return (
+    <>
+      <Home />; oi
+      <p>oi</p>
+    </>
+  );
+}
+
+export default App;
---
+++ front-copinhaIN/src/admin/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import HeaderAdmin from "../../components/header/header";
+
+export default function Home() {
+  return (
+    <>
+      <HeaderAdmin />
+      <h1>Home page</h1>
+    </>
+  );
+}
