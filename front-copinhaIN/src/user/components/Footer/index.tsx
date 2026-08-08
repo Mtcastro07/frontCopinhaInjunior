020a809c3728815a064fcb34663e6b4d75b88cbf
Pedro Amarilio <pedroamarilio@id.uff.br>
2026-08-08 03:08:14 -0300

    style: Estilização da página inicial

---
+++ front-copinhaIN/src/user/components/Footer/index.tsx
@@ -0,0 +1,18 @@
+//import logoImage from '../../../assets/logo1.png'
+import styles from './styles.module.css'
+//import { NavLink } from 'react-router-dom'
+
+export default function Footer() {
+    return (
+        <div className={styles.footer}>
+            <div className={styles.esq}>
+                <h3>COP<span>{'{IN}'}</span>HA  © 2026</h3>
+            </div>
+            <div>
+                <p>Copa do Mundo</p>
+                <p>Fase de Grupos</p>
+                <link href="/admin">Área Administrativa</link>
+            </div>
+        </div>
+    )
+}
